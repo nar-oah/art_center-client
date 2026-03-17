87d0520a4bd284cc4748df5f92841baf78b8c58c
Win10 <givefacegod@gmail.com>
2026-03-17 11:16:35 +0800

    新增自动运行脚本

---
+++ main.bat
@@ -0,0 +1,24 @@
+@echo off
+TITLE Art Center Client
+
+echo Activating Python virtual environment...
+CALL .\venv\Scripts\activate.bat
+
+IF ERRORLEVEL 1 (
+    echo.
+    echo ERROR: Failed to activate Python virtual environment.
+    echo Please make sure the 'venv' folder exists in the current directory.
+    pause
+    exit /b
+)
+
+echo Starting the Python indexing script...
+echo.
+python main.py
+
+echo.
+echo -----------------------------------------------------
+echo Script finished. You can close this window now.
+echo -----------------------------------------------------
+
+pause
